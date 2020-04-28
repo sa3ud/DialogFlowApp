5c3c5f132094761c488c7914d5bdef7e9fb44b31
Yousuf <josephali1993@gmail.com>
2020-04-28 17:00:55 +0400

    #Repackaging
    +Internet Access permissions
    +InternetConnection > check internet connection

--- app/src/main/java/com/programining/dialogflowapp/activities/MainActivity.java
+++ app/src/main/java/com/programining/dialogflowapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.programining.dialogflowapp;
+package com.programining.dialogflowapp.activities;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.programining.dialogflowapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
